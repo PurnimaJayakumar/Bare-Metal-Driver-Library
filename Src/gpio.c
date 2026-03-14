89f114902a28b3272fdeb346af73072d688e9f5b
VighneshSagare <sagarevighnesh0707@gmail.com>
2026-03-14 19:49:41 +0530

    Create gpio.c

---
+++ Src/gpio.c
@@ -0,0 +1,25 @@
+#include "gpio.h"
+
+/* Initialize GPIO pin */
+void GPIO_Init(GPIO_TypeDef *port, uint8_t pin, uint8_t mode)
+{
+    // Implementation will be added later
+}
+
+/* Write value to GPIO pin */
+void GPIO_WritePin(GPIO_TypeDef *port, uint8_t pin, uint8_t value)
+{
+    // Implementation will be added later
+}
+
+/* Read value from GPIO pin */
+uint8_t GPIO_ReadPin(GPIO_TypeDef *port, uint8_t pin)
+{
+    return 0;
+}
+
+/* Toggle GPIO pin */
+void GPIO_TogglePin(GPIO_TypeDef *port, uint8_t pin)
+{
+    // Implementation will be added later
+}
