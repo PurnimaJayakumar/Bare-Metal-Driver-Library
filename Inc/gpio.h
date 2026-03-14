650dbc58dd755d19fe77ba58a0a80c6359ea02ec
VighneshSagare <sagarevighnesh0707@gmail.com>
2026-03-14 22:35:52 +0530

    Update gpio.h

--- Inc/gpio.h
+++ Inc/gpio.h
@@ -30,12 +30,7 @@
 
 void GPIO_EnableClock(GPIO_TypeDef *port);
 
-void GPIO_Init(GPIO_TypeDef *port,
-               uint8_t pin,
-               uint8_t mode,
-               uint8_t otype,
-               uint8_t pull,
-               uint8_t speed);
+void GPIO_Init(GPIO_TypeDef *port, uint8_t pin, uint8_t mode, uint8_t otype, uint8_t pull, uint8_t speed);
 
 void GPIO_WritePin(GPIO_TypeDef *port, uint8_t pin, uint8_t value);
 
