0c3d1ddb7b2b4f00ba841a83d08067ccb5ac3fb6
NARAYANAGARIVARSHITHA <153912751+NARAYANAGARIVARSHITHA@users.noreply.github.com>
2026-03-21 15:16:19 +0530

    Update timer.c

--- timer/Inc/timer.c
+++ timer/Inc/timer.c
@@ -1,70 +1,111 @@
 #include "ti_mer.h"
 
-/* BASIC TIMER CONFIGURATION */
-void timer_basic_init(TIM_TypeDef *TIMx, uint32_t prescaler, uint32_t arr)
-{
-    /* Enable clock for timer */
-    if(TIMx == TIM2)
-        RCC->APB1ENR1 |= RCC_APB1ENR1_TIM2EN;
-    else if(TIMx == TIM3)
-        RCC->APB1ENR1 |= RCC_APB1ENR1_TIM3EN;
+/* Callback pointer */
+static void (*Timer_Callback)(void) = 0;
 
-    TIMx->PSC = prescaler;   // Prescaler
-    TIMx->ARR = arr;         // Auto reload register
-    TIMx->CNT = 0;
+/* Enable Timer Clock */
+static void Timer_EnableClock(TIM_TypeDef *TIMx)
+{
+    if (TIMx == TIM2)
+        RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+    else if (TIMx == TIM3)
+        RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
 }
 
-/* START TIMER */
-void timer_start(TIM_TypeDef *TIMx)
+/* INIT */
+void Timer_Init(TIM_TypeDef *TIMx, Timer_Config_t *config)
+{
+    Timer_EnableClock(TIMx);
+
+    TIMx->PSC = config->prescaler;
+    TIMx->ARR = config->arr;
+    TIMx->CNT = 0;
+
+    /* Apply immediately */
+    TIMx->EGR |= TIM_EGR_UG;
+}
+
+/* START */
+void Timer_Start(TIM_TypeDef *TIMx)
 {
     TIMx->CR1 |= TIM_CR1_CEN;
 }
 
-/* STOP TIMER */
-void timer_stop(TIM_TypeDef *TIMx)
+/* STOP */
+void Timer_Stop(TIM_TypeDef *TIMx)
 {
     TIMx->CR1 &= ~TIM_CR1_CEN;
 }
 
-/* ENABLE PERIODIC INTERRUPT */
-void timer_enable_interrupt(TIM_TypeDef *TIMx)
+/* RESET */
+void Timer_Reset(TIM_TypeDef *TIMx)
+{
+    TIMx->CNT = 0;
+}
+
+/* INTERRUPT ENABLE */
+void Timer_EnableInterrupt(TIM_TypeDef *TIMx)
 {
     TIMx->DIER |= TIM_DIER_UIE;
 
-    if(TIMx == TIM2)
+    if (TIMx == TIM2)
         NVIC_EnableIRQ(TIM2_IRQn);
-    else if(TIMx == TIM3)
+    else if (TIMx == TIM3)
         NVIC_EnableIRQ(TIM3_IRQn);
 }
 
-/* PWM USING TIM3 CHANNEL 1 */
-void timer_pwm_init(uint32_t prescaler, uint32_t arr, uint32_t duty)
+/* SET CALLBACK */
+void Timer_SetCallback(void (*callback)(void))
 {
-    /* Enable clocks */
-    RCC->APB1ENR1 |= RCC_APB1ENR1_TIM3EN;
-    RCC->AHB2ENR  |= RCC_AHB2ENR_GPIOAEN;
+    Timer_Callback = callback;
+}
 
-    /* PA6 alternate function mode */
-    GPIOA->MODER &= ~(3 << (6*2));
-    GPIOA->MODER |=  (2 << (6*2));
+/* ISR */
+void TIM2_IRQHandler(void)
+{
+    if (TIM2->SR & TIM_SR_UIF)
+    {
+        TIM2->SR &= ~TIM_SR_UIF;
 
-    /* AF2 for TIM3 */
-    GPIOA->AFR[0] &= ~(0xF << (6*4));
-    GPIOA->AFR[0] |=  (2 << (6*4));
+        if (Timer_Callback)
+            Timer_Callback();
+    }
+}
 
-    /* Timer configuration */
-    TIM3->PSC = prescaler;
-    TIM3->ARR = arr;
+void TIM3_IRQHandler(void)
+{
+    if (TIM3->SR & TIM_SR_UIF)
+    {
+        TIM3->SR &= ~TIM_SR_UIF;
 
-    /* Duty cycle */
-    TIM3->CCR1 = duty;
+        if (Timer_Callback)
+            Timer_Callback();
+    }
+}
 
-    /* PWM mode 1 */
-    TIM3->CCMR1 |= (6 << 4);
+/* PWM INIT */
+void Timer_PWM_Init(TIM_TypeDef *TIMx, Timer_Config_t *config)
+{
+    Timer_EnableClock(TIMx);
 
-    /* Enable channel */
-    TIM3->CCER |= TIM_CCER_CC1E;
+    TIMx->PSC = config->prescaler;
+    TIMx->ARR = config->arr;
 
-    /* Start timer */
-    TIM3->CR1 |= TIM_CR1_CEN;
+    /* PWM Mode 1 */
+    TIMx->CCMR1 |= (6 << 4);
+
+    /* Preload enable */
+    TIMx->CCMR1 |= TIM_CCMR1_OC1PE;
+    TIMx->CR1   |= TIM_CR1_ARPE;
+
+    /* Enable channel 1 */
+    TIMx->CCER |= TIM_CCER_CC1E;
+
+    TIMx->EGR |= TIM_EGR_UG;
+}
+
+/* SET DUTY (0–100%) */
+void Timer_PWM_SetDuty(TIM_TypeDef *TIMx, uint32_t duty)
+{
+    TIMx->CCR1 = (TIMx->ARR * duty) / 100;
 }
